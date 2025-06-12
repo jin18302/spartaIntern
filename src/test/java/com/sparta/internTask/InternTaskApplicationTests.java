64a5f06f1acaafdee0f6e39a3d434c4cca4fee8f
lee sujin <jin18302@naver.com>
2025-06-12 19:19:00 +0900

    test : 데이터베이스 테스트 관련 수정

--- src/test/java/com/sparta/internTask/InternTaskApplicationTests.java
+++ src/test/java/com/sparta/internTask/InternTaskApplicationTests.java
@@ -3,7 +3,6 @@ package com.sparta.internTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InternTaskApplicationTests {
 
 	@Test
