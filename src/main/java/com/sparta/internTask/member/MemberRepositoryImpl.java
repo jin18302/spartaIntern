53ac7c192863f367c40cc0f8aa8a5449d141fb2b
lee sujin <jin18302@naver.com>
2025-06-12 13:58:15 +0900

    chore: 레포지토리 인터페이스도입

---
+++ src/main/java/com/sparta/internTask/member/MemberRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.sparta.internTask.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemberRepositoryImpl implements MemberRepository{
+    private final Map<Long, Member> inMemory = new HashMap<>();
+    private Long id = 1L;
+
+    public Member save(Member member){
+        member.setId(id++);
+        inMemory.put(member.getId(), member);
+        return member;
+    }
+
+    public boolean existNickName(String nickName){
+        long count = inMemory.values().stream().filter(m -> m.getNickName().equals(nickName)).count();
+        return count != 0;
+    }
+
+    public boolean existPhoneNumber(String phoneNumber){
+        long count = inMemory.values().stream().filter(m -> m.getPhoneNumber().equals(phoneNumber)).count();
+        return count != 0;
+    }
+
+    public boolean existEmail(String email){
+        long count = inMemory.values().stream().filter(m -> m.getEmail().equals(email)).count();
+        return count != 0;
+    }
+
+    public Optional<Member> findByEmail(String email){
+        return inMemory.values().stream().filter(m -> m.getEmail().equals(email)).findFirst();
+    }
+}
